393db418c18b51dd5fdd22e173031bc358c98657
nanocodes64 <nanocodes64@gmail.com>
2023-11-29 21:38:32 +0100

    Cleaned up lexer

--- lexer.py
+++ lexer.py
@@ -1,58 +1,3 @@
-num_i = "num_i"
-num_f = "num_f"
-string = "str"
-identifier = "id"
-quotation = '"'
-keyword, keywords = "key", ["prnt", "altv", "defn"]
-
-#these are functions, prnt being print, altv altering variables, defn defining vars
-
-
-#PRIMITIVES ->
-def _string(line):
-  ret = ""  # just placeholder
-
-  i = 1
-
-  while i < len(line):
-    rn = line[i]
-    if rn == quotation:
-      break
-    ret += rn
-    i += 1
-
-  return ret, string, len(ret)
-
-
-def _numeral(line):
-
-  ret = ""
-
-  for i in line:
-    if not i.isdigit():
-      break
-    ret += i
-
-  return int(ret), num_i, len(ret)
-
-
-def _identifier(line):
-
-  ret = ""
-
-  for i in line:
-    if not i.isalpha():
-      break
-    ret += i
-
-  if ret in keywords:
-    return ret, keyword, len(ret)
-  return ret, identifier, len(ret)
-
-
-# <- PRIMITIVES
-
-
 class TOKEN:
 
   def __init__(self, type, value):
@@ -61,12 +6,17 @@ class TOKEN:
   def __repr__(self):
     return f"TOKEN(TYPE:{self.type}, VALUE:{self.value})"
 
-
 class LEXER:
+
+  num_i = "int"
+  #num_f = "float" # UNUSED ATM!!
+  string = "str"
+  identifier = "id"
+  quotation = '"'
 
   def __init__(self):
     pass
-
+  
   def lexLine(self, line):  #returns array of tokens
     curr_chr = 0
 
@@ -74,11 +24,11 @@ class LEXER:
     while curr_chr < len(line):
       chr_rn = line[curr_chr]
       if chr_rn.isalpha():
-        token, type, chr_passed = _identifier(line[curr_chr:])
+        token, type, chr_passed = self._identifier(line[curr_chr:])
       elif chr_rn.isdigit():
-        token, type, chr_passed = _numeral(line[curr_chr:])
-      elif chr_rn == quotation:
-        token, type, chr_passed = _string(line[curr_chr:])
+        token, type, chr_passed = self._numeral(line[curr_chr:])
+      elif chr_rn == self.quotation:
+        token, type, chr_passed = self._string(line[curr_chr:])
         chr_passed = chr_passed + 2
       else:
         curr_chr += 1
@@ -88,5 +38,42 @@ class LEXER:
       curr_chr += chr_passed
 
     return ret_tokens
+  
+  def _string(self, line):
+    ret = ""  # just placeholder
 
+    i = 1
+
+    while i < len(line):
+      rn = line[i]
+      if rn == self.quotation:
+        break
+      ret += rn
+      i += 1
+
+    return ret, self.string, len(ret)
+
+
+  def _numeral(self, line):
+
+    ret = ""
+
+    for i in line:
+      if not i.isdigit():
+        break
+      ret += i
+
+    return int(ret), self.num_i, len(ret)
+
+
+  def _identifier(self, line):
+
+    ret = ""
+
+    for i in line:
+      if not i.isalpha():
+        break
+      ret += i
+
+    return ret, self.identifier, len(ret)
 
